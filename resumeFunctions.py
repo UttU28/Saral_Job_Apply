716c229d57794b3afc703b2a75aaca101c9c5e26
UttU28 <utsavmaan28@gmail.com>
2024-01-24 15:32:28 -0600

    Initial commit

---
+++ resumeFunctions.py
@@ -0,0 +1,129 @@
+import pyautogui as py
+
+def writeData(data):
+    py.typewrite(data)
+
+
+def firstName():
+    writeData("Utsav")
+    pass
+def lastName():
+    writeData("Chaudhary")
+    pass
+def phone():
+    writeData("6072969583")
+    pass
+def email():
+    writeData("uchaudh3@binghamton.edu")
+    pass
+def fullName():
+    writeData("Utsav Chaudhary")
+    pass
+def linkedIn():
+    writeData("https://www.linkedin.com/in/utsav-chaudhary-875096123/")
+    pass
+
+def addressLine1():
+    writeData("11 Catherine St.")
+    pass
+def city():
+    writeData("Binghamton")
+    pass
+def state():
+    writeData("NY")
+    pass
+def country():
+    writeData("United States")
+    pass
+def zipCode():
+    writeData("13905")
+    pass
+def password():
+    writeData("Application@123z")
+    pass
+
+def startDate1():
+    writeData("09/2021")
+    pass
+def endDate1():
+    writeData("12/2021")
+    pass
+def companyName1():
+    writeData("VRoKCs")
+    pass
+def title1():
+    writeData("Machine Learning Intern")
+    pass
+def describe1():
+    writeData("""● Developed a project based on translating and augmenting Sign Language using ML and Augmented Reality.\n● Utilized MobileNet V2 as a transfer learning model and fine-tuned it to meet the specific needs of the project.\n● Designed a TensorFlow Lite model based on algorithms that demonstrated promising results, achieving 28mAP.\n● Collaborated with developers to integrate the model with augmented reality, creating a seamless user experience.""")
+    pass
+
+def startDate2():
+    writeData("01/2021")
+    pass
+def endDate2():
+    writeData("04/2021")
+    pass
+def companyName2():
+    writeData("Compendious Medialabs Pvt. Ltd")
+    pass
+def title2():
+    writeData("Software Engineering Intern")
+    pass
+def describe2():
+    writeData("""● Developed, designed, and coded a Python bot to aggregate data from over 200 news websites.\n● Implemented logic to retrieve articles from RSS feeds, scraped and sanitized them to align with the database design.\n● Managed and structured data, uploaded it to a MySQL server, and connected it to the frontend.""")
+    pass
+
+def startDate3():
+    writeData("06/2019")
+    pass
+def endDate3():
+    writeData("07/2019")
+    pass
+def companyName3():
+    writeData("underDOGS Gaming Studio")
+    pass
+def title3():
+    writeData(" Game Developer Intern")
+    pass
+def describe3():
+    writeData("""● Integrated a universal gaming currency called "Quarters" into various games for the client utilizing the Quarters API.\n● Incorporated the API into a universal platform game and utilized secure methods for coin transactions.\n● Improved existing code to reduce bugs, resulting in a smoother-running product, using C# and Unity3D.""")
+    pass
+
+def universityName():
+    writeData("Binghamton University")
+    pass
+def major():
+    writeData("Computer Science")
+    pass
+def degree():
+    writeData("MS")
+    pass
+def grad():
+    writeData("MAY 2024")
+    pass
+
+def courseName1():
+    writeData("Python for Everybody Specialization")
+    pass
+def courseInstitute1():
+    writeData("University of Michigan")
+    pass
+def courseName2():
+    writeData("Google IT Automation with Python Specializations")
+    pass
+def courseInstitute2():
+    writeData("Google")
+    pass
+def courseName3():
+    writeData("DeepLearning.AI TensorFlow Developer Specializations")
+    pass
+def courseInstitute3():
+    writeData("DeepLearning.AI")
+    pass
+def courseName4():
+    writeData("Data Structures and Algorithms Specializations (Ongoing)")
+    pass
+def courseInstitute4():
+    writeData("University of California San Diego")
+    pass
