d21553711c888bd998d846b0706d65ac3fba1a9d
UttU28 <utsavmaan28@gmail.com>
2024-06-07 12:08:47 -0500

    Now Its New

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 from resumeFunctions import *
 
-fNames = ["firstName", "lastName", "phone", "email", "fullName", "linkedIn", "addressLine1", "city", "state", "country", "zipCode", "password", "startDate1", "endDate1", "companyName1", "title1", "describe1", "startDate2", "endDate2", "companyName2", "title2", "describe2", "startDate3", "endDate3", "companyName3", "title3", "describe3", "universityName", "major", "degree", "grad", "courseName1", "courseInstitute1", "courseName2", "courseInstitute2", "courseName3", "courseInstitute3", "courseName4", "courseInstitute4"]
-fValues = [firstName, lastName, phone, email, fullName, linkedIn, addressLine1, city, state, country, zipCode, password, startDate1, endDate1, companyName1, title1, describe1, startDate2, endDate2, companyName2, title2, describe2, startDate3, endDate3, companyName3, title3, describe3, universityName, major, degree, grad, courseName1, courseInstitute1, courseName2, courseInstitute2, courseName3, courseInstitute3, courseName4, courseInstitute4]
+fNames = ["firstName", "lastName", "phone", "email", "fullName", "linkedIn", "addressLine1", "city", "state", "country", "zipCode", "password", "startDate1", "endDate1", "companyName1", "title1", "describe1", "startDate2", "endDate2", "companyName2", "title2", "describe2", "startDate3", "endDate3", "companyName3", "title3", "describe3", "startDate4", "endDate4", "companyName4", "title4", "describe4", "universityName", "major", "degree", "grad", "courseName1", "courseInstitute1", "courseName2", "courseInstitute2", "courseName3", "courseInstitute3", "courseName4", "courseInstitute4"]
+fValues = [firstName, lastName, phone, email, fullName, linkedIn, addressLine1, city, state, country, zipCode, password, startDate1, endDate1, companyName1, title1, describe1, startDate2, endDate2, companyName2, title2, describe2, startDate3, endDate3, companyName3, title3, describe3, startDate4, endDate4, companyName4, title4, describe4, universityName, major, degree, grad, courseName1, courseInstitute1, courseName2, courseInstitute2, courseName3, courseInstitute3, courseName4, courseInstitute4]
 
 app = Flask(__name__)
 
--- resumeFunctions.py
+++ resumeFunctions.py
@@ -11,23 +11,23 @@ def lastName():
     writeData("Chaudhary")
     pass
 def phone():
-    writeData("6072969583")
+    writeData("2819680345")
     pass
 def email():
-    writeData("utsavmaan28@gmail.com")
+    writeData("utsav28.devops@gmail.com")
     pass
 def fullName():
     writeData("Utsav Chaudhary")
     pass
 def linkedIn():
-    writeData("https://www.linkedin.com/in/utsav-chaudhary-875096123/")
+    writeData("https://www.linkedin.com/in/utsavmaan28/")
     pass
 
 def addressLine1():
-    writeData("416 Summit Ave.,")
+    writeData("2707 Wild Larch Cir.,")
     pass
 def city():
-    writeData("Arlington")
+    writeData("Katy")
     pass
 def state():
     writeData("TX")
@@ -36,58 +36,74 @@ def country():
     writeData("United States")
     pass
 def zipCode():
-    writeData("76013")
+    writeData("77493")
     pass
 def password():
     writeData("Application@123z")
     pass
 
 def startDate1():
-    writeData("09/2021")
+    writeData("01/2023")
     pass
 def endDate1():
     writeData("12/2021")
     pass
 def companyName1():
-    writeData("VRoKCs")
+    writeData("TransAmerica")
     pass
 def title1():
-    writeData("Machine Learning Intern")
+    writeData("Azure/Cloud DevOps Engineer")
     pass
 def describe1():
-    writeData("""● Developed a project based on translating and augmenting Sign Language using ML and Augmented Reality.\n● Utilized MobileNet V2 as a transfer learning model and fine-tuned it to meet the specific needs of the project.\n● Designed a TensorFlow Lite model based on algorithms that demonstrated promising results, achieving 28mAP.\n● Collaborated with developers to integrate the model with augmented reality, creating a seamless user experience.""")
+    writeData("""Developed and maintained custom Python, Bash, Shell, and PowerShell scripts to automate CI/CD pipelines, enabling seamless integration and deployment processes across Azure DevOps, enhancing build, test, and release efficiency. Using CRON Jobs to Schedule jobs on the master node. \n Utilized Docker to containerize applications, enabling portability and scalability across different environments. Configured Docker images to encapsulate application code, dependencies, and configurations, and stored them in Azure Container Registry, facilitating version control and artifact management. \n Utilized Helm charts to efficiently deploy and manage Kubernetes applications on Azure Kubernetes Service (AKS), leveraging Docker to containerize our applications and Azure Container Registry (ACR) to securely store and manage Docker images. \n Created and managed Azure AD tenants, manage users and groups in Azure AD, and configure application integration with Azure Default Directory. Configured Multi-Factor authentication MFA and aligned single sign-on SSO. \n Established a branching strategy using Git and Azure Repos, creating separate branches for Dev, Test, UAT, and Production environments. \n Created and maintained detailed documentation of migration processes, CI/CD pipeline configurations, and infrastructure setups to facilitate knowledge transfer and future maintenance.""")
     pass
 
 def startDate2():
-    writeData("01/2021")
+    writeData("07/2020")
     pass
 def endDate2():
-    writeData("04/2021")
+    writeData("07/2022")
     pass
 def companyName2():
-    writeData("Compendious Medialabs Pvt. Ltd")
+    writeData("Softline Solutions PVT. LTD.")
     pass
 def title2():
     writeData("Software Engineering Intern")
     pass
 def describe2():
-    writeData("""● Developed, designed, and coded a Python bot to aggregate data from over 200 news websites.\n● Implemented logic to retrieve articles from RSS feeds, scraped and sanitized them to align with the database design.\n● Managed and structured data, uploaded it to a MySQL server, and connected it to the frontend.""")
+    writeData("""Designed cloud native solutions with Azure PAAS services leveraging Azure storage, Azure SQL. Designed event-driven architectures using serverless functions, orchestrated and managed through GitHub Actions. \n Orchestrated infrastructure deployment using both Terraform and Bicep templates, ensuring infrastructure as code principles and efficient resource management. \n Assisted in Network Traffic splitting mechanism using Azure Traffic Manager and Azure Application Gateway, directing users to specific application versions based on predefined rules. \n Configured AKS with Horizontal Pod Autoscaler and Kubernetes Cluster Autoscaler for automated load balancing and scalability, ensuring optimal performance under varying loads. \n Created and managed organization's infrastructure within Azure using Terraform and Ansible, automated deployment processes with GitHub Actions. \n Optimized hosting costs by leveraging Azure Reserved Instances and Azure Hybrid Benefit, maximizing utilization of Azure services while minimizing expenses. \n Wrote Ansible playbooks, Roles and followed best practices to utilize Ansible Handlers with multiple tasks to trigger various handlers and to decouple handlers from their names.""")
     pass
 
 def startDate3():
-    writeData("06/2019")
+    writeData("12/2019")
     pass
 def endDate3():
-    writeData("07/2019")
+    writeData("06/2020")
     pass
 def companyName3():
-    writeData("underDOGS Gaming Studio")
+    writeData("Compendious Medialabs Pvt. Ltd.")
     pass
 def title3():
-    writeData(" Game Developer Intern")
+    writeData("Ansible/Cloud Engineer (I)")
     pass
 def describe3():
-    writeData("""● Integrated a universal gaming currency called "Quarters" into various games for the client utilizing the Quarters API.\n● Incorporated the API into a universal platform game and utilized secure methods for coin transactions.\n● Improved existing code to reduce bugs, resulting in a smoother-running product, using C# and Unity3D.""")
+    writeData("""Developed custom Ansible Modules in Python to enhance functionality and automate tasks within the infrastructure. \n Worked on in-line scripts automation for Ansible and Terraform, using Bash, Python, Shell, and Powershell. \n Developed and maintained scripts to automate repetitive tasks, enhancing productivity and efficiency across the team using Python, Bash, and PowerShell. \n Used Ansible to automate Configuration Management tasks, ensuring consistency, repeatability and avoiding configuration drift across environments. \n Collaborated with fellow DevOps engineers to support the development and maintenance of CI/CD pipelines. \n Tested Jenkins for CI and automated scheduled data scraping and uploading to databases, ensuring timely and accurate data updates. \n Assisted in the deployment and monitoring of automated solutions, ensuring smooth operation and quick issue resolution. \n Knowledge and hands-on experience with popular DevOps tools such as Docker, Kubernetes, Jenkins, Ansible, and Terraform. \n Experience with Infrastructure as Code (IaC) tools, particularly Azure Resource Manager (ARM) templates and Terraform and Ansible for automating configuration management and deployment tasks. \n Strong communication skills for effective interaction with various stakeholders, including developers, operations teams, and management. \n Gained experience in critical thinking and problem-solving abilities, essential for troubleshooting issues and optimizing processes.""")
+    pass
+
+def startDate4():
+    writeData("05/2019")
+    pass
+def endDate4():
+    writeData("12/2019")
+    pass
+def companyName4():
+    writeData("Compendious Medialabs Pvt. Ltd.")
+    pass
+def title4():
+    writeData("Python Engineer (I)")
+    pass
+def describe4():
+    writeData("""Developed Python scripts to automate tasks using APIs, including creation and utilization, and implemented web scraping and testing with Selenium. \n Utilized Python libraries such as requests for efficient handling of HTTP requests for data collection. \n Used libraries like Pandas, NumPy for sorting and filtering data and used Databases for storing, and retrieving data. \n Leveraged Redis for caching and optimizing data retrieval processes, enhancing system performance and efficiency. \n Integrated RSS feed handling into Python scripts, enabling streamlined data processing and utilization for various applications. \n Utilized Azure services for virtual machines, databases, and other infrastructure requirements, ensuring scalable and reliable automation solutions. \n Implemented end-to-end automation workflows, ensuring seamless execution of tasks from data acquisition to storage and processing. \n Contributed to the development of internal tools to streamline repetitive tasks, increasing team productivity and efficiency.""")
     pass
 
 def universityName():
@@ -104,26 +120,26 @@ def grad():
     pass
 
 def courseName1():
-    writeData("Python for Everybody Specialization")
+    writeData("AZ-204 Azure Developer Associate")
     pass
 def courseInstitute1():
-    writeData("University of Michigan")
+    writeData("Microsoft")
     pass
 def courseName2():
-    writeData("Google IT Automation with Python Specializations")
+    writeData("AZ-400 Designing and Implementing Microsoft DevOps Solutions")
     pass
 def courseInstitute2():
-    writeData("Google")
+    writeData("Microsoft")
     pass
 def courseName3():
-    writeData("DeepLearning.AI TensorFlow Developer Specializations")
+    writeData("Python for Everybody Specialization")
     pass
 def courseInstitute3():
-    writeData("DeepLearning.AI")
+    writeData("University of Michigan")
     pass
 def courseName4():
-    writeData("Data Structures and Algorithms Specializations (Ongoing)")
+    writeData("Google IT Automation with Python Specializations")
     pass
 def courseInstitute4():
-    writeData("University of California San Diego")
+    writeData("Google")
     pass
