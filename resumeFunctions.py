f8610eae7215a13f0efaec08f7d693471d357d0f
UttU28 <utsavmaan28@gmail.com>
2024-07-10 19:37:33 -0500

    JSK

--- resumeFunctions.py
+++ resumeFunctions.py
@@ -11,16 +11,16 @@ def lastName():
     writeData("Chaudhary")
     pass
 def phone():
-    writeData("2819680345")
+    writeData("6072969583")
     pass
 def email():
-    writeData("utsav28.devops@gmail.com")
+    writeData("utsavmaan28@gmail.com")
     pass
 def fullName():
     writeData("Utsav Chaudhary")
     pass
 def linkedIn():
-    writeData("https://www.linkedin.com/in/utsavmaan28/")
+    writeData("https://thatinsaneguy28.netlify.app/")
     pass
 
 def addressLine1():
@@ -43,35 +43,35 @@ def password():
     pass
 
 def startDate1():
-    writeData("01/2023")
+    writeData("02/2023")
     pass
 def endDate1():
     writeData("12/2021")
     pass
 def companyName1():
-    writeData("TransAmerica")
+    writeData("Midh Tech")
     pass
 def title1():
-    writeData("Azure/Cloud DevOps Engineer")
+    writeData("Python Web Software Developer - DevOps")
     pass
 def describe1():
-    writeData("""‣ Developed and maintained custom Python, Bash, Shell, and PowerShell scripts to automate CI/CD pipelines, enabling seamless integration and deployment processes across Azure DevOps, enhancing build, test, and release efficiency. Using CRON Jobs to Schedule jobs on the master node. \n‣ Utilized Docker to containerize applications, enabling portability and scalability across different environments. Configured Docker images to encapsulate application code, dependencies, and configurations, and stored them in Azure Container Registry, facilitating version control and artifact management. \n‣ Utilized Helm charts to efficiently deploy and manage Kubernetes applications on Azure Kubernetes Service (AKS), leveraging Docker to containerize our applications and Azure Container Registry (ACR) to securely store and manage Docker images. \n‣ Created and managed Azure AD tenants, manage users and groups in Azure AD, and configure application integration with Azure Default Directory. Configured Multi-Factor authentication MFA and aligned single sign-on SSO. \n‣ Established a branching strategy using Git and Azure Repos, creating separate branches for Dev, Test, UAT, and Production environments. \n‣ Created and maintained detailed documentation of migration processes, CI/CD pipeline configurations, and infrastructure setups to facilitate knowledge transfer and future maintenance.""")
+    writeData("""‣ Developed high-performance RESTful APIs using Python and Flask framework on Azure Cloud, for finance data transactions. \n‣ Developed JSON-based services adhering to JSON Schema specification for precise validation and structuring of financial data payloads, ensuring data integrity and compliance. Integrated Azure Redis Cache for implementing caching strategies. \n‣ Architected scalable and secure backend services using Flask within Azure App Service, deploying RESTful APIs and integrating OAuth 2.0 authentication with role-based access control (RBAC). \n‣ Designed and implemented interactive web interfaces using HTML, CSS, JavaScript, and jQuery, optimizing user interaction and visualization of financial data dashboards and reporting tools. \n‣ Created comprehensive test cases using tools like pytest and unittest to validate functionality, performance, and security, Integrating the test cases in the DevOps Pipeline build for testing environment. Utilized Postman for rigorous API testing.""")
     pass
 
 def startDate2():
-    writeData("07/2020")
+    writeData("09/2021")
     pass
 def endDate2():
-    writeData("07/2022")
+    writeData("08/2022")
     pass
 def companyName2():
-    writeData("Softline Solutions PVT. LTD.")
+    writeData("Compendious Medialabs Pvt. Ltd.")
     pass
 def title2():
-    writeData("Cloud DevOps Engineer")
+    writeData("Python Data Engineer (I)")
     pass
 def describe2():
-    writeData("""‣ Designed cloud native solutions with Azure PAAS services leveraging Azure storage, Azure SQL. Designed event-driven architectures using serverless functions, orchestrated and managed through GitHub Actions. \n‣ Orchestrated infrastructure deployment using both Terraform and Bicep templates, ensuring infrastructure as code principles and efficient resource management. \n‣ Assisted in Network Traffic splitting mechanism using Azure Traffic Manager and Azure Application Gateway, directing users to specific application versions based on predefined rules. \n‣ Configured AKS with Horizontal Pod Autoscaler and Kubernetes Cluster Autoscaler for automated load balancing and scalability, ensuring optimal performance under varying loads. \n‣ Created and managed organization's infrastructure within Azure using Terraform and Ansible, automated deployment processes with GitHub Actions. \n‣ Optimized hosting costs by leveraging Azure Reserved Instances and Azure Hybrid Benefit, maximizing utilization of Azure services while minimizing expenses. \n‣ Wrote Ansible playbooks, Roles and followed best practices to utilize Ansible Handlers with multiple tasks to trigger various handlers and to decouple handlers from their names.""")
+    writeData("""‣ Developed robust Python scripts for automated data scraping of news feeds using libraries such as BeautifulSoup and Scrapy. Implemented and developed Selenium Headless in AWS EC2, for scheduling additional data extraction of news from sources. \n‣ Tested and Implemented data filtering techniques in Python utilizing NumPy and Pandas for cleaning and preprocessing data. \n‣ Developed a data dashboard using the Django framework in Python, integrating dynamic data updates from DynamoDB. We utilized D3.js and Chart.js for creating interactive and live charts and graphs based on filters. \n‣ Assisted in developing a scalable scraping bot on AWS Lambda, integrating with CloudWatch for scheduling and monitoring. \n‣ Supported Natural Language Processing (NLP) tasks in Python, using NLTK and spaCy to classify news articles by genre and sentiment, NLTK and spaCy to classify news articles, enhancing skills in text analysis and categorization.""")
     pass
 
 def startDate3():
@@ -81,13 +81,13 @@ def endDate3():
     writeData("06/2020")
     pass
 def companyName3():
-    writeData("Compendious Medialabs Pvt. Ltd.")
+    writeData("underDOGS Gaming Studio")
     pass
 def title3():
-    writeData("Ansible/Cloud Engineer (I)")
+    writeData("Python Backend / Game Developer (I)")
     pass
 def describe3():
-    writeData("""‣ Developed custom Ansible Modules in Python to enhance functionality and automate tasks within the infrastructure. \n‣ Worked on in-line scripts automation for Ansible and Terraform, using Bash, Python, Shell, and Powershell. \n‣ Developed and maintained scripts to automate repetitive tasks, enhancing productivity and efficiency across the team using Python, Bash, and PowerShell. \n‣ Used Ansible to automate Configuration Management tasks, ensuring consistency, repeatability and avoiding configuration drift across environments. \n‣ Collaborated with fellow DevOps engineers to support the development and maintenance of CI/CD pipelines. \n‣ Tested Jenkins for CI and automated scheduled data scraping and uploading to databases, ensuring timely and accurate data updates. \n‣ Assisted in the deployment and monitoring of automated solutions, ensuring smooth operation and quick issue resolution. \n‣ Knowledge and hands-on experience with popular DevOps tools such as Docker, Kubernetes, Jenkins, Ansible, and Terraform. \n‣ Experience with Infrastructure as Code (IaC) tools, particularly Azure Resource Manager (ARM) templates and Terraform and Ansible for automating configuration management and deployment tasks. \n‣ Strong communication skills for effective interaction with various stakeholders, including developers, operations teams, and management. \n‣ Gained experience in critical thinking and problem-solving abilities, essential for troubleshooting issues and optimizing processes.""")
+    writeData("""‣ Integrated and optimized the Quarters API across multiple games, facilitating seamless universal gaming currency transactions as part of a POC and creating player wallets, conducting secure transactions, converting points to quarters. \n‣ Developed Python scripts to integrate third-party APIs for cloud storage, social media sharing, and payment gateways. \n‣ Assisted in implementing secure transaction methods integrating with Unity3D, leveraging API best practices to safeguard in-game currency transactions and protect user data privacy. Identified and resolved critical bugs in existing API codebases. \n‣ Tested API integration improving real-time data sync and communication between clients and servers using Python and C#. \n‣ Helped in optimizing API response times by refactoring code and implementing caching strategies, and implemented unit tests to validate API functionalities, ensuring reliability and minimizing regression issues across iterative game updates.""")
     pass
 
 def startDate4():
@@ -97,13 +97,13 @@ def endDate4():
     writeData("12/2019")
     pass
 def companyName4():
-    writeData("Compendious Medialabs Pvt. Ltd.")
+    writeData("xxx")
     pass
 def title4():
-    writeData("Python Engineer (I)")
+    writeData("ccc")
     pass
 def describe4():
-    writeData("""‣ Developed Python scripts to automate tasks using APIs, including creation and utilization, and implemented web scraping and testing with Selenium. \n‣ Utilized Python libraries such as requests for efficient handling of HTTP requests for data collection. \n‣ Used libraries like Pandas, NumPy for sorting and filtering data and used Databases for storing, and retrieving data. \n‣ Leveraged Redis for caching and optimizing data retrieval processes, enhancing system performance and efficiency. \n‣ Integrated RSS feed handling into Python scripts, enabling streamlined data processing and utilization for various applications. \n‣ Utilized Azure services for virtual machines, databases, and other infrastructure requirements, ensuring scalable and reliable automation solutions. \n‣ Implemented end-to-end automation workflows, ensuring seamless execution of tasks from data acquisition to storage and processing. \n‣ Contributed to the development of internal tools to streamline repetitive tasks, increasing team productivity and efficiency.""")
+    writeData("""ccc""")
     pass
 
 def universityName():
